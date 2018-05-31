631e451300971e5296a6e884320f1b5735a5bcee
mjens12 <mjens12@gmail.com>
2018-05-31 12:43:28 -0400

    Merging with lab4 stuff

--- src/package1/SuperTicTacToeGame.java
+++ src/package1/SuperTicTacToeGame.java
@@ -1,32 +1,89 @@
 package package1;
 
 public class SuperTicTacToeGame {
-	public static final int BDSIZE = 3;
 	private CellStatus[][] board;
 	private GameStatus status;
+	private int size;
+	private CellStatus turn;
+	private int connections; // used in to do 4.
 
-	public SuperTicTacToeGame() {
+	public SuperTicTacToeGame(int size) {
 		status = GameStatus.IN_PROGRESS;
-		board = new CellStatus[BDSIZE][BDSIZE];
+		this.size = size;
+		this.connections = 3;
+		board = new CellStatus[size][size];
+		turn = CellStatus.X;
+		reset();
+	}
 
-		for (int row = 0; row < BDSIZE; row++)
-			for (int col = 0; col < BDSIZE; col++)
-				board[row][col] = CellStatus.EMPTY;
+	public CellStatus[][] getBoard() {
+		return board;
 	}
 
 	public void select(int row, int col) {
+		if (board[row][col] != CellStatus.EMPTY)
+			return;
+
+		board[row][col] = turn;
+
+		if (turn == CellStatus.O)
+			turn = CellStatus.X;
+		else
+			turn = CellStatus.O;
+		status = isWinner();
+	}
+
+	private GameStatus isWinner() {
+
+		for (int r = 0; r < 3; r++) {
+			if (board[r][0] == CellStatus.X
+					&& board[r][1] == CellStatus.X
+					&& board[r][2] == CellStatus.X) {
+				return GameStatus.X_WON;
+			}
+		}
+
+		for (int c = 0; c < 3; c++) {
+			if (board[0][c] == CellStatus.O
+					&& board[1][c] == CellStatus.O
+					&& board[2][c] == CellStatus.O) {
+				return GameStatus.O_WON;
+			}
+		}
+
+		/*
+		 * TODO 4: Change all code to allow any size board (i.e., not
+		 * just 3) Change the code from TODO 3 to allow a horizontal win
+		 * of any length, defined by 'this.connections' (see above)
+		 * (Hint: you will also need to think about how this.connections
+		 * should be set)
+		 */
+
+		return GameStatus.IN_PROGRESS;
 
 	}
 
-	public void reset() {
+	/*
+	 * TO DO 5, if you have the time...
+	 *
+	 * Make an Undo feature.
+	 */
 
+	public boolean undo() {
+		return false;
 	}
 
 	public GameStatus getGameStatus() {
-
+		return status;
 	}
 
-	public CellStatus getCell(int row, int col) {
-		return board[row][col];
+	public void reset() {
+		for (int r = 0; r < size; r++)
+			for (int c = 0; c < size; c++)
+				board[r][c] = CellStatus.EMPTY;
+	}
+
+	public boolean getOK(int r, int c) {
+		return board[r][c] == CellStatus.EMPTY;
 	}
 }
--- src/package1/SuperTicTacToePanel.java
+++ src/package1/SuperTicTacToePanel.java
@@ -1,77 +1,151 @@
 package package1;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 public class SuperTicTacToePanel extends JPanel {
+
 	private JButton[][] board;
-	private CellStatus iCell;
+	private CellStatus[][] iBoard;
+	private JLabel xWon;
+	private JLabel oWon;
 	private JButton quitButton;
+	private JButton undoButton;
+
+	private JMenuItem gameItem;
+	private JMenuItem quitItem;
+
 	private SuperTicTacToeGame game;
 
+	private ImageIcon emptyIcon;
 	private ImageIcon xIcon;
 	private ImageIcon oIcon;
-	private ImageIcon emptyIcon;
 
-	private void SuperTicTacToePanel() {
-		quitButton = new JButton("Quit Game");
+	public SuperTicTacToePanel(JMenuItem pquitItem,
+			JMenuItem pgameItem) {
 
-		JPanel panel1 = new JPanel();
-		JPanel panel2 = new JPanel();
+		gameItem = pgameItem;
+		quitItem = pquitItem;
 
-		ButtonListener buttonListen = new ButtonListener();
+		JPanel bottom = new JPanel();
+		JPanel center = new JPanel();
 
-		// TODO need these image files
-		xIcon = new ImageIcon("x.jpg");
-		oIcon = new ImageIcon("y.jpg");
-		emptyIcon = new ImageIcon("empty.jpg");
+		xIcon = new ImageIcon("src/lab4/X.png");
+		oIcon = new ImageIcon("src/lab4/O.png");
+		emptyIcon = new ImageIcon("src/lab4/empty.png");
 
-		GridLayout layout = new GridLayout(3, 3);
+		// create game, listeners
+		ButtonListener listener = new ButtonListener();
 
-		game = new SuperTicTacToeGame();
+		game = new SuperTicTacToeGame(3);
 
-		panel1.setLayout(layout);
+		// create Undo and quit buttons.
+		quitButton = new JButton("Quit");
+		quitButton.addActionListener(listener);
+		undoButton = new JButton("Undo");
+		undoButton.addActionListener(listener);
 
-		for (int row = 0; row < SuperTicTacToeGame.BDSIZE; row++)
-			for (int col = 0; col < SuperTicTacToeGame.BDSIZE; col++) {
-				board[row][col] = new JButton("", emptyIcon);
-				board[row][col].addActionListener(buttonListen);
-				panel1.add(board[row][col]);
+		// create the board
+		center.setLayout(new GridLayout(3, 3, 3, 2));
+		Dimension temp = new Dimension(100, 100);
+		board = new JButton[3][3];
+
+		for (int row = 0; row < 3; row++)
+			for (int col = 0; col < 3; col++) {
+
+				Border thickBorder = new LineBorder(Color.blue, 2);
+
+				board[row][col] = new JButton(" ");
+				board[row][col].setBorder(thickBorder);
+
+				board[row][col].addActionListener(listener);
+				board[row][col].setPreferredSize(temp);
+				center.add(board[row][col]);
 			}
+
+		displayBoard();
+
+		bottom.setLayout(new GridLayout(3, 2));
+
+		JLabel labxWins = new JLabel("X Wins: ");
+		JLabel laboWins = new JLabel("O Wins: ");
+		xWon = new JLabel("0");
+		oWon = new JLabel("0");
+
+		bottom.add(labxWins);
+		bottom.add(xWon);
+		bottom.add(laboWins);
+		bottom.add(oWon);
+		bottom.add(quitButton);
+		bottom.add(undoButton);
+
+		// add all to contentPane
+
+		add(new JLabel("!!!!!!  Super TicTacToe  !!!!!!"),
+				BorderLayout.NORTH);
+		add(center, BorderLayout.CENTER);
+		add(bottom, BorderLayout.SOUTH);
+
 	}
 
 	private void displayBoard() {
-		for (int row = 0; row < SuperTicTacToeGame.BDSIZE; row++)
-			for (int col = 0; col < SuperTicTacToeGame.BDSIZE; col++) {
-				iCell = game.getCell(row, col);
-				// TODO: ImageIcon icon = icon for iCell
-				board[row][col].setIcon(icon);
+		System.out.println(GameStatus.O_WON);
+		iBoard = game.getBoard();
+
+		for (int r = 0; r < board.length; r++)
+			for (int c = 0; c < board.length; c++) {
+
+				board[r][c].setText("");
+				if (iBoard[r][c] == CellStatus.O)
+					board[r][c].setIcon(oIcon);
+
+				else if (iBoard[r][c] == CellStatus.X)
+					board[r][c].setIcon(xIcon);
+
+				else if (iBoard[r][c] == CellStatus.EMPTY)
+					board[r][c].setIcon(emptyIcon);
 			}
+
 	}
 
 	private class ButtonListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
-			for (int row = 0; row < SuperTicTacToeGame.BDSIZE; row++)
-				for (int col = 0; col < SuperTicTacToeGame.BDSIZE; col++)
-					if (board[row][col] == e.getSource())
-						// tell the game which button was selected.
-						game.select(row, col);
+			if (quitButton == e.getSource())
+				System.exit(0);
+
+			for (int r = 0; r < board.length; r++)
+				for (int c = 0; c < board.length; c++)
+					if (board[r][c] == e.getSource()
+							&& game.getOK(r, c))
+						game.select(r, c);
 
 			displayBoard();
 
+			// Note: This code only checks if O wins-- no other win
+			// conditions
+			// The other win conditions should be added later
 			if (game.getGameStatus() == GameStatus.O_WON) {
 				JOptionPane.showMessageDialog(null,
 						"O won and X lost!\n The game will reset");
+				game.reset();
+				displayBoard();
+				oWon.setText(
+						"" + (Integer.parseInt(oWon.getText()) + 1));
 			}
 		}
 	}
 }
-// HELLO TEST 2
-// Max's test
